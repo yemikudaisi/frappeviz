b1672843a3f3cd9c84a6fa5518902e6fef2ed37d
Yemi Kudaisi <yemikudaisi@gmail.com>
2020-09-08 11:13:05 +0100

    refactor: merged code to one source file

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import argparse
 import os
 import sys
 from sys import modules
-from umlbuilder import ClassGenerator
 import json
 
 # Create the parser
@@ -50,6 +49,78 @@ def is_frappe_app_folder(path_to_app):
         return True
     
     return False
+
+class Composition:
+    """ Python class for composition class relartionship
+    """
+    def __init__(self, class_name):
+        self.owner_class_name = class_name
+
+    def has(self, other_class_name, comment = ''):
+        """Assign composition relationship between one class to another
+        """
+        self.child_class_name = other_class_name
+        self.comment = comment
+        return self
+    
+    def __str__(self):
+        output = '"%s" *-- "%s"' % (self.child_class_name,self.owner_class_name)
+        if not self.comment == '':
+            output += ': %s' % (self.comment)
+
+        return output
+
+class Extension:
+    """ Python class for extension class relartionship
+    """
+    def __init__(self, class_name):
+        self.child_class_name = class_name
+
+    def extends(self, other_class_name, comment = ''):
+        self.parent_class_name = other_class_name
+        self.comment = comment
+        return self
+    
+    def __str__(self):
+        return '"%s" <|- "%s" : %s' % (self.parent_class_name,self.child_class_name, self.comment)
+
+class Docfield:
+    """ Python representing docfield in a frappe doctype
+    """
+    def __init__(self, name, type_name):
+        self.name = name
+        self.type_name = type_name
+    
+    def __str__(self):
+        return '%s : %s' % (self.name, self.type_name)
+
+class ClassGenerator:
+    """Doctype class diagram UML generator
+    """
+    def __init__(self, class_name):
+        self.class_name = class_name
+        self.fields = []
+        self.relationships = []
+    
+    def addField(self, fieldObj):
+        """ Adds a docfield to a class"""
+        if fieldObj['fieldtype'] == 'Link':
+            self.relationships.append(Composition(self.class_name).has(fieldObj['options'],fieldObj['fieldname']))
+        self.fields.append(Docfield(fieldObj['fieldname'], fieldObj['fieldtype']))
+    
+    def to_plantuml(self):
+        """ Returns PlantUML class diagram for doctype in text format
+        """
+        output = ''
+
+        for r in self.relationships:
+            output += '\n'+str(r)
+
+        output += '\n  class "%s" {' % (self.class_name)
+        for f in self.fields:
+            output += '\n    '+str(f)
+        output += '\n  }\n'
+        return output
 
 def generate_doctype_uml(doctype_name, fields):
     """Generates class diagram for doctype given a list of fields
